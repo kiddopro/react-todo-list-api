fd1e0ca062bdeed2c0714a8a111d138d6ed38642
Martín Suárez <ms.prgramador@gmail.com>
2021-09-18 02:04:28 +0000

    almost

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [tasksList, setTasksList] = useState([]);
+
+	function addTask() {
+		setTasksList([...tasksList, task]);
+	}
+
+	function deleteTask(index) {
+		let newList = tasksList.filter(e => {
+			return e.key != index;
+		});
+
+		// setTasksList(newList);
+	}
+
+	function keyHandler(key) {
+		if (key == "Enter") {
+			addTask();
+			setTask("");
+		}
+	}
+
 	return (
-		<div className="text-center mt-5">
-			
+		<div className="text-left mt-5 mx-auto w-50">
+			<div className="taskContainer">
+				<h1 className="text-center">todos</h1>
+				<input
+					type="text"
+					className="form-control"
+					value={task}
+					onChange={e => setTask(e.target.value)}
+					onKeyPress={a => keyHandler(a.key)}
+					placeholder="What do you need to be done?"
+				/>
+				<ul>
+					{tasksList.map((item, index) => {
+						return (
+							<li key={index}>
+								{item}
+								<span onClick={deleteTask(index)}>❌</span>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };
