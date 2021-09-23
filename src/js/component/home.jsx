4d3c68548e98e8af30af8a0d4521419575bd7ffd
Martín Suárez <ms.prgramador@gmail.com>
2021-09-23 02:34:59 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -25,34 +25,42 @@ const Home = () => {
 	}
 
 	return (
-		<div className="text-left mt-5 mx-auto w-50 bg-light p-1">
-			<div className="taskContainer">
-				<h1 className="text-center">todos</h1>
-				<input
-					type="text"
-					className="form-control border-0"
-					value={task}
-					onChange={e => setTask(e.target.value)}
-					onKeyPress={a => keyHandler(a.key)}
-					placeholder="What do you need to be done?"
-				/>
-				<ul>
-					{tasksList.map((item, index) => {
-						return (
-							<li key={index} className="form-control items">
-								{item}
-								<span onClick={() => deleteTask(index)}>
-									✖️
-								</span>
-							</li>
-						);
-					})}
-				</ul>
+		<>
+			<div className="text-left mt-5 mx-auto w-50 bg-light p-1 border">
+				<div className="taskContainer">
+					<h1 className="text-center">todos</h1>
+					<input
+						type="text"
+						className="form-control border-0"
+						value={task}
+						onChange={e => setTask(e.target.value)}
+						onKeyPress={a => keyHandler(a.key)}
+						placeholder="What do you need to be done?"
+					/>
+					<ul>
+						{tasksList.map((item, index) => {
+							return (
+								<li key={index} className="form-control items">
+									{item}
+									<span onClick={() => deleteTask(index)}>
+										✖️
+									</span>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div>
+					<small>{tasksList.length} remaining </small>
+				</div>
 			</div>
-			<div>
-				<small>{tasksList.length} remaining </small>
-			</div>
-		</div>
+			<div
+				className="bg-light mx-auto border border-top-0"
+				style={{ width: "49%", height: "5px" }}></div>
+			<div
+				className="bg-light mx-auto border border-top-0"
+				style={{ width: "48%", height: "5px" }}></div>
+		</>
 	);
 };
 
